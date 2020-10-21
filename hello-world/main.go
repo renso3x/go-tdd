86c8fab2abc3da4f9f6b81af6ba03459e64bb123
renso3x <enso123456@gmail.com>
2020-10-21 10:01:38 +0800

    extending test with switch case and function

--- hello-world/main.go
+++ hello-world/main.go
@@ -1,18 +1,33 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Printf(Hello("world"))
+	fmt.Printf(Hello("world", "English"))
 }
